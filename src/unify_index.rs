d2c088fb062f8d3d8417e33bbc66bc8787281395
Nobuyuki <n.umetani@gmail.com>
2023-03-04 08:26:40 +0900

    made the unfiy_index function robust

--- src/unify_index.rs
+++ src/unify_index.rs
@@ -15,27 +15,44 @@ pub fn unify_separate_trimesh_indexing_xyz_uv(
     let mut tri2uni = vec!(usize::MAX; num_tri * 3);
     let mut uni2vtx_uv = Vec::<usize>::new();
     let mut uni2vtx_xyz = Vec::<usize>::new();
-    for itri in 0..num_tri {
+
+    for i_tri in 0..num_tri {
         for i_node in 0..3 {
-            if tri2uni[itri * 3 + i_node] != usize::MAX { continue; }
-            let ivtx_xyz = tri2vtx_xyz[itri * 3 + i_node];
-            let ivtx_uv = tri2vtx_uv[itri * 3 + i_node];
-            let s0 = &vtx_xyz2tri.1[vtx_xyz2tri.0[ivtx_xyz]..vtx_xyz2tri.0[ivtx_xyz + 1]];
-            let s1 = &vtx_uv2tri.1[vtx_uv2tri.0[ivtx_uv]..vtx_uv2tri.0[ivtx_uv + 1]];
+            let i_vtx_uv = tri2vtx_uv[i_tri * 3 + i_node];
+            let s1 = &vtx_uv2tri.1[vtx_uv2tri.0[i_vtx_uv]..vtx_uv2tri.0[i_vtx_uv + 1]];
+            for jtri0 in s1.into_iter() {
+                let jtri = *jtri0;
+                let jno = crate::tri2vtx::find_index_tri(&tri2vtx_uv[jtri*3..jtri*3+3], i_vtx_uv);
+                assert_eq!(tri2vtx_uv[jtri * 3 + jno], i_vtx_uv);
+            }
+        }
+    }
+
+    for i_tri in 0..num_tri {
+        for i_node in 0..3 {
+            if tri2uni[i_tri * 3 + i_node] != usize::MAX { continue; }
+            let i_vtx_xyz = tri2vtx_xyz[i_tri * 3 + i_node];
+            let i_vtx_uv = tri2vtx_uv[i_tri * 3 + i_node];
+            let s0 = &vtx_xyz2tri.1[vtx_xyz2tri.0[i_vtx_xyz]..vtx_xyz2tri.0[i_vtx_xyz + 1]];
+            let s1 = &vtx_uv2tri.1[vtx_uv2tri.0[i_vtx_uv]..vtx_uv2tri.0[i_vtx_uv + 1]];
             let s0 = std::collections::BTreeSet::<&usize>::from_iter(s0.iter());
             let s1 = std::collections::BTreeSet::<&usize>::from_iter(s1.iter());
             let intersection: Vec<_> = s0.intersection(&s1).collect();
             if intersection.is_empty() { continue; }
-            let iuni = uni2vtx_uv.len();
-            uni2vtx_xyz.push(ivtx_xyz);
-            uni2vtx_uv.push(ivtx_uv);
-            for jtri0 in intersection.into_iter() {
-                let jtri = *jtri0;
-                let jno = crate::tri2vtx::find_index_tri(&tri2vtx_xyz[jtri*3..jtri*3+3], ivtx_xyz);
-                assert_eq!(tri2vtx_xyz[jtri * 3 + jno], ivtx_xyz);
-                assert_eq!(tri2vtx_uv[jtri * 3 + jno], ivtx_uv);
-                assert_eq!(tri2uni[jtri * 3 + jno], usize::MAX);
-                tri2uni[jtri * 3 + jno] = iuni;
+            let i_uni = uni2vtx_uv.len(); // new unified vertex
+            assert_eq!(uni2vtx_uv.len(), uni2vtx_xyz.len());
+            uni2vtx_xyz.push(i_vtx_xyz);
+            uni2vtx_uv.push(i_vtx_uv);
+            for j_tri in intersection.into_iter().cloned() {
+                let j_node_xyz = crate::tri2vtx::find_index_tri(&tri2vtx_xyz[j_tri *3..j_tri *3+3], i_vtx_xyz);
+                assert_eq!(tri2vtx_xyz[j_tri * 3 + j_node_xyz], i_vtx_xyz);
+                let j_node_uv = crate::tri2vtx::find_index_tri(&tri2vtx_uv[j_tri *3..j_tri *3+3], i_vtx_uv);
+                assert_eq!(tri2vtx_uv[j_tri * 3 + j_node_uv], i_vtx_uv);
+                if j_node_xyz != j_node_uv {  // this sometime happens
+                    continue;
+                }
+                assert_eq!(tri2uni[j_tri * 3 + j_node_xyz], usize::MAX);
+                tri2uni[j_tri * 3 + j_node_xyz] = i_uni;
             }
         }
     }
