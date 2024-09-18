3379f2a8992859d27a3039e6551219da8a2e7489
Nobyuki Umetani <n.umetani@gmail.com>
2024-09-19 07:09:03 +0900

    hoge

--- del-msh-core/src/io_ply.rs
+++ del-msh-core/src/io_ply.rs
@@ -8,6 +8,7 @@ enum Format {
 /* --------------------------------------*/
 // below: XyzRgb
 
+/*
 pub struct XyzRgb {
     pub xyz: [f64; 3],
     pub rgb: [u8; 3],
@@ -18,23 +19,36 @@ impl crate::vtx2xyz::HasXyz<f64> for XyzRgb {
         self.xyz
     }
 }
+ */
 
-pub fn read_xyzrgb<P: AsRef<std::path::Path>>(path: P) -> anyhow::Result<Vec<XyzRgb>> {
+pub trait Xyz {
+    fn set_xyz(&mut self, xyz: &[f64;3]);
+}
+
+pub trait Rgb {
+    fn set_rgb(&mut self, rgb: &[u8;3]);
+}
+
+
+
+pub fn read_xyzrgb<Path: AsRef<std::path::Path>, XyzRgb: Xyz + Rgb + Default>(path: Path) -> anyhow::Result<Vec<XyzRgb>> {
     // let file_path = "C:/Users/nobuy/Downloads/juice_box.ply";
     // let file_path = "/Users/nobuyuki/project/juice_box1.ply";
     let file = std::fs::File::open(path)?;
     let mut reader = std::io::BufReader::new(file);
     use std::io::BufRead;
     let mut line = String::new();
-    let hoge = reader.read_line(&mut line)?;
+    let _hoge = reader.read_line(&mut line)?;
     assert_eq!(line, "ply\n");
     line.clear();
-    let hoge = reader.read_line(&mut line)?;
+    let _hoge = reader.read_line(&mut line)?;
     dbg!(&line);
     let strs: Vec<_> = line.split_whitespace().collect();
     assert_eq!(strs[0], "format");
-    let format = match strs[1] {
+    let _format = match strs[1] {
         "binary_little_endian" => Format::BinaryLittleEndian,
+        "binary_big_endian" => Format::BinaryBigEndian,
+        "ascii" => Format::Ascii,
         &_ => panic!(),
     };
     let _ = reader.read_line(&mut line)?;
@@ -62,7 +76,7 @@ pub fn read_xyzrgb<P: AsRef<std::path::Path>>(path: P) -> anyhow::Result<Vec<Xyz
     //
     let mut buf: Vec<u8> = Vec::new();
     reader.read_to_end(&mut buf)?;
-    let mut vtx2xyzrgb: Vec<XyzRgb> = vec![];
+    let mut pnt2xyzrgb: Vec<XyzRgb> = vec![];
     for i_elem in 0..num_elem {
         let i_byte: usize = i_elem * (8 * 3 + 3);
         let x = f64::from_le_bytes(buf[i_byte..i_byte + 8].try_into()?);
@@ -72,10 +86,10 @@ pub fn read_xyzrgb<P: AsRef<std::path::Path>>(path: P) -> anyhow::Result<Vec<Xyz
         let g = u8::from_le_bytes(buf[i_byte + 25..i_byte + 26].try_into()?);
         let b = u8::from_le_bytes(buf[i_byte + 26..i_byte + 27].try_into()?);
         // dbg!((i_byte, i_elem, i_elem * (8 * 3 + 3), x,y,z));
-        vtx2xyzrgb.push(XyzRgb {
-            xyz: [x, y, z],
-            rgb: [r, g, b],
-        });
+        let mut pnt = XyzRgb::default();
+        pnt.set_xyz(&[x,y,z]);
+        pnt.set_rgb(&[r,g,b]);
+        pnt2xyzrgb.push(pnt);
     }
-    Ok(vtx2xyzrgb)
+    Ok(pnt2xyzrgb)
 }
