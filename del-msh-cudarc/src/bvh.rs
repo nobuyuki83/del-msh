2a3a9861855705e3cb9495ccdfb9d95d3d4e517a
Nobyuki Umetani <n.umetani@gmail.com>
2024-11-27 01:15:05 +0900

    ci

--- del-msh-cudarc/src/bvh.rs
+++ del-msh-cudarc/src/bvh.rs
@@ -42,11 +42,8 @@ pub fn tri2cntr_from_trimesh3(
     let num_tri = tri2vtx.len() / 3;
     let cfg = cudarc::driver::LaunchConfig::for_num_elems(num_tri as u32);
     let param = (tri2cntr, num_tri as u32, tri2vtx, vtx2xyz);
-    let from_trimsh = del_cudarc_util::get_or_load_func(
-        dev,
-        "tri2cntr",
-        del_msh_cudarc_kernel::BVHNODES_MORTON,
-    )?;
+    let from_trimsh =
+        del_cudarc_util::get_or_load_func(dev, "tri2cntr", del_msh_cudarc_kernel::BVHNODES_MORTON)?;
     use cudarc::driver::LaunchAsync;
     unsafe { from_trimsh.launch(cfg, param) }?;
     Ok(())
