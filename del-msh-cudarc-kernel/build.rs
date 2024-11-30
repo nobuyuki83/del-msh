3d2cd0082df8d015ac224fb6cdcc94bee268f796
Nobyuki Umetani <n.umetani@gmail.com>
2024-11-30 18:28:48 +0900

    move cpp heaers to del-geo

--- del-msh-cudarc-kernel/build.rs
+++ del-msh-cudarc-kernel/build.rs
@@ -3,8 +3,17 @@ fn main() {
     println!("cargo:rerun-if-changed=src/bvhnode2aabb.cu");
     println!("cargo:rerun-if-changed=src/bvhnodes_morton.cu");
     println!("cargo:rerun-if-changed=src/aabb3_from_vtx2xyz.cu");
-
-    let builder = bindgen_cuda::Builder::default().include_paths_glob("../cpp_header/*");
+    //
+    let path_out_dir = std::env::var("OUT_DIR").unwrap();
+    let path_out_dir = std::path::Path::new(&path_out_dir).join("cpp_headers");
+    // dbg!(&path_out_dir);
+    std::fs::create_dir_all(&path_out_dir).unwrap();
+    del_geo_cpp_headers::HEADERS.write_files(&path_out_dir);
+    let glob_input = path_out_dir.join("*.h").into_os_string().into_string().unwrap();
+    // dbg!(&glob_input);
+    let builder = bindgen_cuda::Builder::default().include_paths_glob(&glob_input);
+    //
+    // let builder = bindgen_cuda::Builder::default().include_paths_glob("../cpp_header/*");
     println!("cargo:info={builder:?}");
     let bindings = builder.build_ptx().unwrap();
     bindings.write("src/lib.rs").unwrap();
