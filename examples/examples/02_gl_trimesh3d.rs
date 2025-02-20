4ccae9832b19b5c02f88b10c32b7ceb75433b1c0
Nobuyuki Umetani <n.umetani@gail.com>
2025-02-20 12:50:15 +0900

    small

---
+++ examples/examples/02_gl_trimesh3d.rs
@@ -0,0 +1,51 @@
+use del_gl_core::gl;
+
+struct MyViewTrg {
+    drawer: del_gl_core::drawer_mesh::Drawer,
+}
+
+impl del_gl_winit_glutin::viewer3d_for_gl_renderer::GlRenderer for MyViewTrg {
+    fn draw(&mut self, gl: &gl::Gl, cam_model: &[f32; 16], cam_projection: &[f32; 16]) {
+        self.drawer.draw(&gl, &cam_model, &cam_projection);
+    }
+
+    fn initialize(&mut self, gl: &gl::Gl) {
+        self.drawer.compile_shader(&gl);
+        let (tri2vtx, vtx2xyz) = {
+            let mut obj = del_msh_core::io_obj::WavefrontObj::<usize, f32>::new();
+            obj.load("asset/spot/spot_triangulated.obj").unwrap();
+            (obj.idx2vtx_xyz, obj.vtx2xyz)
+        };
+        let edge2vtx = del_msh_core::edge2vtx::from_triangle_mesh(&tri2vtx, vtx2xyz.len() / 3);
+        self.drawer.update_vertex(&gl, &vtx2xyz, 3);
+        self.drawer
+            .add_element(&gl, gl::TRIANGLES, &tri2vtx, [1.0, 0.0, 0.0]);
+        self.drawer
+            .add_element(&gl, gl::LINES, &edge2vtx, [0.0, 0.0, 0.0]);
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let window_attributes = winit::window::Window::default_attributes()
+        .with_transparent(false)
+        .with_title("02_meshtex")
+        .with_inner_size(winit::dpi::PhysicalSize {
+            width: 600,
+            height: 600,
+        });
+    let template = glutin::config::ConfigTemplateBuilder::new()
+        .with_alpha_size(8)
+        .with_transparency(cfg!(cgl_backend));
+    let display_builder =
+        glutin_winit::DisplayBuilder::new().with_window_attributes(Some(window_attributes));
+    let mut app = del_gl_winit_glutin::viewer3d_for_gl_renderer::Viewer3d::new(
+        template,
+        display_builder,
+        Box::new(MyViewTrg {
+            drawer: del_gl_core::drawer_mesh::Drawer::new(),
+        }),
+    );
+    let event_loop = winit::event_loop::EventLoop::new().unwrap();
+    event_loop.run_app(&mut app)?;
+    app.appi.exit_state
+}
--- examples/examples/1_symmetry_detection.rs
+++ examples/examples/1_symmetry_detection.rs
@@ -1,3 +1,5 @@
+
+
 fn kernel(a: &[f32; 12], b: &[f32; 12], h: f32) -> f32 {
     let dist = del_geo_core::vecn::squared_distance(a, b);
     let dist = dist / (h * h);
@@ -8,31 +10,44 @@ fn kernel(a: &[f32; 12], b: &[f32; 12], h: f32) -> f32 {
     }
 }
 
-/*
-fn dw_kernel(a: &[f32; 12], x: &[f32; 12], h: f32) -> [f32; 12] {
-    let dist = del_geo_core::vecn::distance(a, x);
-    if dist.abs() > 1. {
-        [0f32; 12]
-    } else {
-        use del_geo_core::vecn::VecN;
-        a.scale(-1.5 * dist / (h * h))
-    }
+fn get_normal_and_origin_from_affine_matrix_of_reflection(
+    affine: &[f32; 12],
+) -> ([f32; 3], [f32; 3]) {
+    let (rot, transl): ([f32; 9], [f32; 3]) = (
+        arrayref::array_ref!(affine, 0, 9).clone(),
+        arrayref::array_ref!(affine, 9, 3).clone(),
+    );
+    // dbg!(rot, transl);
+    use del_geo_core::mat3_col_major::Mat3ColMajor;
+    let tmp = rot.sub(&del_geo_core::mat3_col_major::from_identity()); // -2{n}{n}^T
+    let (u, s, v) = del_geo_core::mat3_col_major::svd(
+        &tmp,
+        //del_geo_core::mat3_sym::EigenDecompositionModes::Analytic,
+        del_geo_core::mat3_sym::EigenDecompositionModes::JacobiNumIter(100),
+    )
+    .unwrap();
+    //dbg!(del_geo_core::mat3_col_major::determinant(&u));
+    let (u0, u1, u2) = del_geo_core::mat3_col_major::to_columns(&u);
+    let n = u2;
+    use del_geo_core::vec3::Vec3;
+    let p = n.scale(del_geo_core::vec3::dot(&transl, &u2) * 0.5);
+    (n, p)
 }
- */
 
-fn main() -> anyhow::Result<()> {
+pub fn sym_detector(
+    tri2vtx: &[usize],
+    vtx2xyz: &[f32],
+    i_seed: u64,
+    num_sample: usize,
+) -> [f32; 12] {
     // use del_geo_core::vec3::Vec3;
     use rand::Rng;
     use rand::SeedableRng;
-    let mut rng = rand_chacha::ChaChaRng::seed_from_u64(6);
-    let (tri2vtx, vtx2xyz) = del_msh_core::io_obj::load_tri_mesh::<_, usize, f32>(
-        "../../asset/spot/spot_triangulated.obj",
-        None,
-    )?;
+    let mut rng = rand_chacha::ChaChaRng::seed_from_u64(i_seed);
     let tri2cumsumarea = del_msh_core::trimesh::tri2cumsumarea(&tri2vtx, &vtx2xyz, 3);
-    let num_vtxs = 300;
-    let mut vtxs2xyz = vec![0f32; num_vtxs * 3];
-    for i_vtxs in 0..num_vtxs {
+    let mut sample2xyz = vec![0f32; num_sample * 3];
+    let mut sample2nrm = vec![0f32; num_sample * 3];
+    for i_sample in 0..num_sample {
         let r0 = rng.random::<f32>();
         let r1 = rng.random::<f32>();
         let (i_tri, r0, r1) =
@@ -40,13 +55,18 @@ fn main() -> anyhow::Result<()> {
         let p0 = del_msh_core::trimesh::position_from_barycentric_coordinate::<_, 3>(
             &tri2vtx, &vtx2xyz, i_tri, r0, r1,
         );
-        del_msh_core::vtx2xyz::to_vec3_mut(&mut vtxs2xyz, i_vtxs).copy_from_slice(&p0);
+        let n0 = del_msh_core::trimesh3::to_tri3(&tri2vtx, &vtx2xyz, i_tri).unit_normal();
+        del_msh_core::vtx2xyz::to_vec3_mut(&mut sample2xyz, i_sample).copy_from_slice(&p0);
+        del_msh_core::vtx2xyz::to_vec3_mut(&mut sample2nrm, i_sample).copy_from_slice(&n0);
     }
-    let mut pair2trans = Vec::<[f32; 12]>::with_capacity(num_vtxs * (num_vtxs - 1));
-    for i_vtxs in 0..num_vtxs {
-        let p_i = del_msh_core::vtx2xyz::to_vec3(&vtxs2xyz, i_vtxs);
-        for j_vtxs in i_vtxs + 1..num_vtxs {
-            let p_j = del_msh_core::vtx2xyz::to_vec3(&vtxs2xyz, j_vtxs);
+    let mut pair2trans = Vec::<([f32; 12], usize, usize)>::with_capacity(num_sample * (num_sample - 1));
+    for i_sample in 0..num_sample {
+        let p_i = del_msh_core::vtx2xyz::to_vec3(&sample2xyz, i_sample);
+        let n_i = del_msh_core::vtx2xyz::to_vec3(&sample2nrm, i_sample);
+        for j_sample in i_sample + 1..num_sample {
+            use del_geo_core::mat4_col_major;
+            let p_j = del_msh_core::vtx2xyz::to_vec3(&sample2xyz, j_sample);
+            let n_j = del_msh_core::vtx2xyz::to_vec3(&sample2nrm, j_sample);
             let pm = del_geo_core::edge3::position_from_ratio(p_i, p_j, 0.5);
             use del_geo_core::vec3::Vec3;
             let nrm = p_j.sub(p_i).normalize();
@@ -54,11 +74,14 @@ fn main() -> anyhow::Result<()> {
                 &del_geo_core::mat3_col_major::from_identity(),
                 &del_geo_core::mat3_col_major::from_scaled_outer_product(2., &nrm, &nrm),
             );
-            use del_geo_core::mat4_col_major;
+            {
+                let cos = del_geo_core::mat3_col_major::mult_vec(&r_mat, &n_i).dot(&n_j);
+                if cos < 0.9 { continue; } // filtering out
+            }
             let t_mat = mat4_col_major::mult_three_mats_col_major(
                 &mat4_col_major::from_translate(&pm),
                 &mat4_col_major::from_mat3_col_major_adding_w(&r_mat),
-                &mat4_col_major::from_translate(&[-pm[0], -pm[1], -pm[2]]),
+                &mat4_col_major::from_translate(&pm.scale(-1f32)),
             );
             {
                 let p1_j = mat4_col_major::transform_homogeneous(&t_mat, p_i).unwrap();
@@ -67,29 +90,63 @@ fn main() -> anyhow::Result<()> {
                 assert!(p1_i.sub(p_i).norm() < 1.0e-5);
             }
             let affine = del_geo_core::mat3x4_col_major::from_mat4_col_major(&t_mat);
-            pair2trans.push(affine);
+            pair2trans.push((affine, i_sample, j_sample));
         }
     }
-    let mut cur_trans = {
-        let mut rng = rand_chacha::ChaChaRng::seed_from_u64(1);
-        let i_pair = rng.random_range(0..pair2trans.len());
-        pair2trans[i_pair]
-    };
-    let window_size = 0.8;
-    for _itr in 0..100 {
-        use del_geo_core::vecn::VecN;
-        // compute density and its gradient
-        let mut sum_weight = 0.;
-        let mut sum_transf = [0f32; 12];
-        for trans in &pair2trans {
-            let w = kernel(&cur_trans, trans, window_size);
-            // let dw = dw_kernel(&cur_trans, trans, window_size);
-            sum_weight += w;
-            sum_transf.add_in_place(&trans.scale(w));
+    println!("num pair: {}", pair2trans.len());
+    for _iter in 0..10 {
+        let (mut cur_trans, _, _) = {
+            let i_pair = rng.random_range(0..pair2trans.len());
+            pair2trans[i_pair]
+        };
+        let window_size = 1.0;
+        let mut sum_weight_pre = 0f32;
+        let mut max_weight_and_pair = (0f32, usize::MAX, usize::MAX);
+        for _itr in 0..100 {
+            // TODO: accelerate using Kd-tree
+            use del_geo_core::vecn::VecN;
+            let mut sum_weight = 0.;
+            let mut sum_transf = [0f32; 12];
+            for (trans, i_sample, j_sample) in &pair2trans {
+                let w = kernel(&cur_trans, trans, window_size);
+                sum_weight += w;
+                sum_transf.add_in_place(&trans.scale(w));
+                if w > max_weight_and_pair.0 {
+                    max_weight_and_pair = (w, *i_sample, *j_sample);
+                }
+            }
+            cur_trans = sum_transf.scale(1.0 / sum_weight);
+            if sum_weight < sum_weight_pre * 1.01 { break;}
+            sum_weight_pre = sum_weight_pre;
         }
-        cur_trans = sum_transf.scale(1.0 / sum_weight);
-        dbg!(cur_trans);
-        dbg!(sum_weight);
+        dbg!(cur_trans, max_weight_and_pair);
+        // TODO: region grow algorithm
     }
+    [0f32;12]
+}
+
+fn main() -> anyhow::Result<()> {
+    use del_geo_core::vec3::Vec3;
+    let (tri2vtx, vtx2xyz) = del_msh_core::io_obj::load_tri_mesh::<_, usize, f32>(
+        "asset/spot/spot_triangulated.obj",
+        None,
+    )
+        .unwrap();
+    let affine = sym_detector(&tri2vtx, &vtx2xyz, 9, 200);
+    dbg!(affine);
+    let (n, p) = get_normal_and_origin_from_affine_matrix_of_reflection(&affine);
+    dbg!(n, p);
+    let (ex, ey) = del_geo_core::vec3::basis_xy_from_basis_z(&n);
+    use slice_of_array::SliceFlatExt;
+    let vtx2xyz = [
+        p.sub(&ex).sub(&ey),
+        p.add(&ex).sub(&ey),
+        p.add(&ex).add(&ey),
+        p.sub(&ex).add(&ey),
+    ]
+    .flat()
+    .to_vec();
+    let tri2vtx = [[0usize, 1, 2], [0, 2, 3]].flat().to_vec();
+    del_msh_core::io_obj::save_tri2vtx_vtx2xyz("hoge.obj", &tri2vtx, &vtx2xyz, 3)?;
     Ok(())
 }
